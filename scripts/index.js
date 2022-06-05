6f555bd9d69dc47a2bfc543661877cba536bbc1e
Ivan Kovalenko <gunonu@yandex.ru>
2022-06-05 20:57:38 +0300

    add card popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,63 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button'); 
-let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form')
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let inputName = document.querySelector('.popup__input_value_name');
-let inputDescription = document.querySelector('.popup__input_value_description');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+
+const openAddCardPopupButton = document.querySelector('.profile__add-button');
+const closeAddCardPopupButton = document.querySelector('.popup-add-card__close-button');
+const popupAddCard = document.querySelector('.popup-add-card');
+
+
+const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form')
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const inputName = document.querySelector('.popup__input_value_name');
+const inputDescription = document.querySelector('.popup__input_value_description');
+
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
 
 function openPopup() {
     popup.classList.add('popup_opened');
     inputName.value = profileName.textContent;
     inputDescription.value = profileDescription.textContent;
 }
-
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+
+function openAddCardPopup() {
+    popupAddCard.classList.add('popup_opened');
+}
+function closeAddCardPopup() {
+    popupAddCard.classList.remove('popup_opened');
+}
+
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -25,5 +67,8 @@ function formSubmitHandler(evt) {
 }
 
 openPopupButton.addEventListener('click', openPopup);
+openAddCardPopupButton.addEventListener('click', openAddCardPopup);
+closeAddCardPopupButton.addEventListener('click', closeAddCardPopup);
+
 closePopupButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
